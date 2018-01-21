5a6c806b360fc8e9db2955c23c96ea78c3f63a4e
LIGANG08 <34271422+LIGANG08@users.noreply.github.com>
2018-01-21 20:18:35 +0800

    react-native(navigation导航)

--- struggle/app/navigation.js
+++ struggle/app/navigation.js
@@ -2,59 +2,110 @@
  *  2018-1-21 Li Gang react-navigation
  */
 
-import React from 'react';
-import { Button, Image } from 'react-native';
-import { TabNavigator } from 'react-navigation'; // 1.0.0-beta.14
+// import React from 'react';
+// import { StyleSheet, Button, Image } from 'react-native';
+// import { TabNavigator } from 'react-navigation'; // 1.0.0-beta.14
 
-import home from '../app/pages/Home';
-import person from '../app/pages/Person';
+// import home from '../app/pages/Home';
+// import person from '../app/pages/Person';
 
-class MyHomeScreen extends React.Component {
-  static navigationOptions = {
-    tabBarLabel: 'Home',
-    tabBarIcon: () => {
-      return (
-        <Image
-          source={require('./images/find.png')}
-          style={{ tintColor: '#FF6347', width: 26, height: 26 }}
-        />
-      );
-    },
-  };
+// class MyHomeScreen extends React.Component {
+//   static navigationOptions = {
+//     tabBarLabel: '发现',
+//     tabBarIcon: () => (
+//       <Image
+//         source={require('./images/find.png')}
+//         style={[styles.icon, { tintColor: '#FF6347' }]}
+//       />
+//     ),
+//   };
 
-  render() {
-    return (
-      <Button
-        onPress={() => this.props.navigation.navigate('home')}
-        title="Go to notifications"
-      />
-    );
-  }
-}
+//   render() {
+//     return (
+//       <Button
+//         onPress={() => this.props.navigation.navigate('home')}
+//         title="Go to notifications"
+//       />
+//     );
+//   }
+// }
 
 // const styles = StyleSheet.create({
 //   icon: {
-//     width: 26,
-//     height: 26,
+//     width: 30,
+//     height: 30,
 //   },
 // });
 
-const MyApp = TabNavigator({
-  Home: {
-    screen: MyHomeScreen,
-  },
-  home: {
-    screen: home,
-  },
-  person: {
-    screen: person,
-  },
-}, {
-  tabBarPosition: 'top',
-  animationEnabled: true,
-  tabBarOptions: {
-    activeTintColor: '#e91e63',
+// const MyApp = TabNavigator({
+//   Home: {
+//     screen: MyHomeScreen,
+//   },
+//   column: {
+//     screen: home,
+//   },
+//   person: {
+//     screen: person,
+//   },
+// }, {
+//   tabBarPosition: 'bottom',
+//   animationEnabled: true,
+//   tabBarOptions: {
+//     activeTintColor: '#e91e63',
+//   },
+// });
+
+// export default MyApp;
+
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const HomeScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+  </View>
+);
+
+const ProfileScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Profile Screen</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30,
   },
 });
 
-export default MyApp;
+const RootTabs = TabNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: () => (
+        <Image
+          source={require('./images/find.png')}
+          style={[styles.icon, { tintColor: '#FF6347' }]}
+        />
+      ),
+    },
+  },
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: {
+      tabBarLabel: 'Profile',
+      tabBarIcon: () => (
+        <Image
+          source={require('./images/find.png')}
+          style={[styles.icon, { tintColor: '#FF6347' }]}
+        />
+      ),
+    },
+  },
+});
+
+export default RootTabs;
