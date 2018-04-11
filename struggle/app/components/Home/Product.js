09dc611e7f2d42b13f904b9651bfd3c1c98681b6
LIGANG08 <34271422+LIGANG08@users.noreply.github.com>
2018-04-11 16:49:36 +0800

    reactnative(极客时间App更新)

--- struggle/app/components/Home/Product.js
+++ struggle/app/components/Home/Product.js
@@ -18,36 +18,41 @@ const data = [
     price: 199,
     describe: '让运维体系管理伴你成长',
     auto: '赵成',
+    identity: '美丽联合集团技术服务经理',
   }, {
     key: 2,
-    title: '走向大前端',
+    title: '深入浅出区块链',
     uri: 'https://wx2.sinaimg.cn/mw690/006SQnpEly1flwdx494pyj30yi0lytal.jpg',
     price: 199,
-    describe: '让运维体系管理伴你成长',
-    auto: '赵成',
+    describe: '你的区块链入门第一课',
+    auto: '陈浩',
+    identity: '元界CTO',
   }, {
     key: 3,
-    title: '技术管理',
+    title: 'AI技术内参',
     uri: 'https://wx1.sinaimg.cn/mw690/006SQnpEly1flg7zjcqmaj30yi0kcqay.jpg',
     price: 199,
-    describe: '让运维体系管理伴你成长',
-    auto: '朱赟',
+    describe: '你的360度人工智能信息助理',
+    auto: '洪亮劼',
+    identity: 'Etsy数据科学主管',
   },
   {
     key: 4,
-    title: '从前端小工到 BAT 中高级工程师的必备技能',
+    title: '技术与商业案例解读',
     uri: 'https://static001.geekbang.org/static/www/img/course_qianduan.96ed848.jpg',
     price: 199,
-    describe: '让运维体系管理伴你成长',
-    auto: '杨文坚',
+    describe: '一堂课看懂技术创新与商业模式',
+    auto: '徐飞',
+    identity: '计算机博士,资深架构师',
   },
   {
     key: 5,
-    title: 'TensorFlow实战——基础班',
+    title: '推荐系统三十六式',
     uri: 'https://static001.geekbang.org/static/www/img/course_tensorflow.dd05c11.jpg',
     price: 199,
-    describe: '让运维体系管理伴你成长',
-    auto: '郑泽宇',
+    describe: '解决系统起步阶段80%的问题',
+    auto: '刑无刀',
+    identity: '链家网资深算法专家',
   },
 ];
 
@@ -67,6 +72,7 @@ export default class Product extends Component {
                   <View
                     key={item.key}
                     style={{
+                    display: 'flex',
                     marginLeft: 10,
                     marginTop: 10,
                     borderRadius: 6,
@@ -88,22 +94,21 @@ export default class Product extends Component {
                         }}
                       >
                         <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                          <Text style={styles.text}>{item.title}</Text>
+                          <Text numberOfLines={1} style={styles.text}>{item.title}</Text>
                           <Text style={{ marginTop: 10, color: 'tomato' }}>$ {item.price} / 年</Text>
                         </View>
                         <Text style={{ fontSize: 12, color: 'gray' }}>{item.describe}</Text>
                       </View>
-                      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                         <Image
                           source={{ uri: item.uri }}
                           style={{
                           width: 20,
                           height: 20,
                           borderRadius: 10,
-                          marginTop: 10,
                           }}
                         />
-                        <Text style={styles.text}>{item.auto}<Text style={{ color: 'gray' }}> | Etsy数据科学主管</Text></Text>
+                        <Text style={{ margin: 10 }}>{item.auto}<Text style={{ color: 'gray' }}> | {item.identity}</Text></Text>
                       </View>
                     </View>
                   </View>
@@ -149,6 +154,7 @@ const styles = StyleSheet.create({
 
   text: {
     margin: 10,
+    width: 120,
   },
 
 });
--- struggle/app/pages/Column.js
+++ struggle/app/pages/Column.js
@@ -14,35 +14,72 @@ const data = [
   {
     key: 1,
     title: '赵成的运维体系管理',
+    describe: '带你直击运维的本质',
+    price: 58,
+    time: 36,
+    number: 1608,
     uri: 'https://wx3.sinaimg.cn/crop.0.0.1242.698.1000/006SQnpEly1fmjtm5idmmj30yi0kcwv5.jpg',
     auto: '赵成',
+    identity: '美丽联合集团技术服务经理',
+    update: '更新完毕',
   }, {
     key: 2,
-    title: '走向大前端',
+    title: '人工智能基础课',
+    describe: '通俗易懂的人工智能入门课',
+    price: 58,
+    time: 36,
+    number: 6994,
     uri: 'https://wx2.sinaimg.cn/mw690/006SQnpEly1flwdx494pyj30yi0lytal.jpg',
+    auto: '王天一',
+    identity: '工学博士，副教授',
+    update: '每周一至周五更新',
   }, {
     key: 3,
-    title: '技术管理',
+    title: '朱赞的技术管理',
+    describe: '从技术到管理，让你的目标函数达到最优解',
+    price: 58,
+    time: 36,
+    number: 1600,
     uri: 'https://wx1.sinaimg.cn/mw690/006SQnpEly1flg7zjcqmaj30yi0kcqay.jpg',
     auto: '朱赟',
+    identity: '计算机博士,Airbnb技术经理',
+    update: '每周一 / 三 / 五更新',
   },
   {
     key: 4,
     title: '从前端小工到 BAT 中高级工程师的必备技能',
+    describe: '带你直击运维的本质',
+    price: 58,
+    time: 36,
+    number: 1007,
     uri: 'https://static001.geekbang.org/static/www/img/course_qianduan.96ed848.jpg',
     auto: '杨文坚',
+    identity: '美丽联合集团技术服务经理',
+    update: '更新完毕',
   },
   {
     key: 5,
-    title: 'TensorFlow实战——基础班',
+    title: '邱岳的产品手记',
+    describe: '重构你的产品思维',
+    price: 58,
+    time: 45,
+    number: 3677,
     uri: 'https://static001.geekbang.org/static/www/img/course_tensorflow.dd05c11.jpg',
-    auto: '郑泽宇',
+    auto: '邱岳',
+    identity: '无码科技产品经理，公众号二爷',
+    update: '每周二 / 四更新',
   },
   {
     key: 6,
     title: 'AI技术内参',
+    describe: '你的360度人工智能信息助理',
+    price: 58,
+    time: 40,
+    number: 4323,
     uri: 'https://static001.geekbang.org/resource/image/ac/9e/ac8ee8c95891e59388eb3b7c76431a9e.jpg',
     auto: '洪亮劼',
+    identity: 'Etsy数据科学主管，前雅虎研...',
+    update: '更新完毕',
   },
 ];
 
@@ -79,23 +116,72 @@ export default class Main extends Component {
                       key={item.key}
                       style={{
                       display: 'flex',
-                      padding: 10,
                       flexDirection: 'row',
+                      padding: 10,
                       marginTop: 10,
                       borderRadius: 6,
                       borderColor: '#E8E8E8',
                       borderWidth: 1,
+                      backgroundColor: 'white',
                       }}
                     >
-                      <View>
-                        <Image source={{ uri: item.uri }} style={{ width: 90, height: 90, borderRadius: 6 }} />
-                      </View>
-                      <View style={{ marginLeft: 10 }}>
-                        <View style={{ height: 30 }}>
-                          <Text style={styles.text}>{item.title}</Text>
+                      <View style={{ flex: 2, padding: 10 }}>
+                        <View style={{
+                          borderBottomWidth: 1,
+                          borderBottomColor: 'gray',
+                          display: 'flex',
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                          paddingBottom: 10,
+                          }}
+                        >
+                          <View style={{
+                            width: 36,
+                            height: 36,
+                            borderRadius: 18,
+                            borderWidth: 1,
+                            borderColor: 'tomato',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            }}
+                          >
+                            <Image source={{ uri: item.uri }} style={{ width: 30, height: 30, borderRadius: 15 }} />
+                          </View>
+                          <View style={{ marginLeft: 10 }}>
+                            <Text style={{}}>{item.auto}</Text>
+                            <Text numberOfLines={1} style={{ color: 'gray', marginTop: 3, width: 120 }}>{item.identity}</Text>
+                          </View>
                         </View>
-                        <View style={{ height: 30 }}>
-                          <Text style={styles.text}>{item.auto}</Text>
+                        <View style={{
+                          marginTop: 10,
+                          display: 'flex',
+                          justifyContent: 'center',
+                          }}
+                        >
+                          <Text style={{ fontSize: 15 }}>{item.title}</Text>
+                          <Text style={{ marginTop: 5, fontSize: 12, color: 'gray' }}>{item.describe}</Text>
+                        </View>
+                        <View style={{
+                          marginTop: 20,
+                          display: 'flex',
+                          flexDirection: 'row',
+                          justifyContent: 'space-between',
+                          }}
+                        >
+                          <Text style={{ fontSize: 15, color: 'tomato' }}>$ {item.price} / {item.time}期</Text>
+                          <Text style={{ fontSize: 12, color: 'gray' }}>{item.number}人购买</Text>
+                        </View>
+                      </View>
+                      <View style={{ flex: 1 }}>
+                        <View style={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          }}
+                        >
+                          <Image source={{ uri: item.uri }} style={{ width: 100, height: 100, borderRadius: 6 }} />
+                          <Text style={{ marginTop: 20, fontSize: 12, color: 'gray' }}>{item.update}</Text>
                         </View>
                       </View>
                     </View>
@@ -114,6 +200,7 @@ const styles = StyleSheet.create({
   cont: {
     display: 'flex',
     alignItems: 'center',
+    backgroundColor: '#F0F0F0',
   },
 
   container: {
